60dce488bf609a94c528b231954d7e375aa3b6a3
Gbadamosi Odofin <odofing@gmail.com>
2021-12-14 17:04:06 +0100

    modified again

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,9 +2,7 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import { Row, Col } from 'react-bootstrap'
 import {
-  ImHtmlFive,
   SiNextdotjs,
-  IoLogoCss3,
   SiBootstrap,
   SiJavascript,
   GrReactjs,
